f66b5d2e11bae555d41b46c8162bda415e1ad033
vitalis-virtus <vshayuk@gmail.com>
2024-01-17 08:08:05 +0200

    created empty implementation of the Game interface

--- main.go
+++ main.go
@@ -1,3 +1,24 @@
 package main
 
-func main() {}
+import "github.com/hajimehoshi/ebiten/v2"
+
+type Game struct{}
+
+func (g *Game) Draw(screen *ebiten.Image) {}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Layout(outsideWidth, insideHeight int) (screenWidth, screenHight int) {
+	return outsideWidth, insideHeight
+}
+
+func main() {
+	g := Game{}
+
+	err := ebiten.RunGame(&g)
+	if err != nil {
+		panic(err)
+	}
+}
